466019a7d3df0133e95098bc2845db5a14ca48c8
JawadShk <shaikhjawad020@gmail.com>
2022-10-15 17:11:20 +0530

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,43 @@
 // import logo from './logo.svg';
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, {useState} from 'react'
+// import Alert from './components/Alert';
 
 function App() {
+  const [mode, setMode] = useState('light')
+  // const [alert, setAlert] = useState(null)
+
+  // const showAlert = (message,type) =>{
+  //   setAlert({
+  //     msg: message,
+  //     type: type
+  //   })
+  // } 
+
+  const toggleMode = () =>{
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "#212529";
+      document.body.style.color = "white";
+      // showAlert("Dark Mode Has Been Enabled", "success")
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      // showAlert("Light Mode Has Been Enabled", "success")
+    }
+  }
   return (
     <>
-      <Navbar title= "TestUtils2" aboutText="aboutUs"/>
+      <Navbar title= "TestUtils2" aboutText="aboutUs" mode={mode} toggleMode={toggleMode}/>
+      {/* <Alert alert={alert}/> */}
       <div className="container my-5">
           <TextForm heading="Enter Text To Analyze" />
+          {/* <About /> */}
       </div>
     </>
   );
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -6,20 +6,47 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick = () =>{
+        // console.log("Uppercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleClearClick = () =>{
+        // console.log("Uppercase was clicked" + text);
+        let newText = "";
+        setText(newText);
+    }
     const handleOnChange = (event) =>{
         // console.log("On change");
         setText(event.target.value);
+    }
+    const handleCopy = () =>{
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
     }
     const [text, setText] = useState("Enter text here");
     // text = "new text"; //wrong way of changing state 
     // setText("New Text");
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <>
+            <div className='container'>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                </div>
+                <button type="button" className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to uppercase</button>
+                <button type="button" className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button type="button" className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear Text</button>
+                <button type="button" className="btn btn-primary mx-2 my-1" onClick={handleCopy}>Copy Text</button>
             </div>
-            <button type="button" className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
-        </div>
+            <div className="container my-3">
+                <h2>Your Text Summary</h2>
+                <p> <b>{text.split(" ").length}</b> words and <b>{text.length}</b> characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes Read</p>
+                <h2>Preview</h2>
+                <p>{text.length>0?text:"Enter Your Text To Preview Here"}</p>
+            </div>
+        </>
   )
 }
